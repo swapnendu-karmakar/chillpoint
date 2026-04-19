5062af51e3d4944b53ae814d693939f811fa4527
Swapnendu Karmakar <ktskarmakar@gmail.com>
2026-04-19 15:56:05 +0530

    fix: responsiveness of tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import FeaturesStrip from './components/FeaturesStrip'
@@ -8,6 +9,19 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 export default function App() {
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      setTimeout(() => {
+        const el = document.getElementById(hash.replace('#', ''))
+        if (el) {
+          const y = el.getBoundingClientRect().top + window.scrollY - 70 // Offset for sticky navbar
+          window.scrollTo({ top: y, behavior: 'smooth' })
+        }
+      }, 500) // Wait for layout settling on mobile
+    }
+  }, [])
+
   return (
     <div className="min-h-screen font-body">
       <Navbar />
